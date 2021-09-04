2853f4c6e21cec6f92da02eeb6d39a99d2108a18
vagrant <vagrant@vagrant.vm>
2021-09-04 20:48:40 +0000

    15-likes-model-and-admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,9 @@
 
-from django.db import models
 from django.contrib.auth.models import User
 from utils.time_helpers import utc_now
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from likes.models import Like
 
 
 class Tweet(models.Model):
@@ -18,6 +20,14 @@ class Tweet(models.Model):
         # datetime.now 不带时区信息，需要增加上 utc 的时区信息
         return (utc_now() - self.created_at).seconds // 3600
 
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Tweet),
+            object_id=self.id,
+        ).order_by('-created_at')
+
     def __str__(self):
         # 这里是你执行 print(tweet instance) 的时候会显示的内容
         return f'{self.created_at} {self.user}: {self.content}'
+
